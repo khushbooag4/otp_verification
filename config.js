54d95e93d2e6692675f444181fc7c589948cb340
Khushboo <kushbooagarwal3584@gmail.com>
2022-01-11 12:56:51 +0530

    Commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    authToken : "0f21238635d6e1faa2bb18518c87f7a6",
+    serviceID: "VA446d4da1a442f3d8030c1298b3777fef",
+    accountSID: "ACb30c91880d11c0dce38c9afa22aa2da4"
+}
---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const config = require('./config');
+
+const app = express()
+const port = 3000
+
+const client = require('twilio')(config.accountSID,config.authToken)
+
+app.get('/', (req, res)=>{
+    res.status(200).send("Hello World")
+})
+
+// Login Endpoint
+app.get('/auth', (req,res) => {
+     if (req.query.mobilenumber) {
+        client
+        .verify
+        .services(config.serviceID)
+        .verifications
+        .create({
+            to: `+${req.query.mobilenumber}`,
+            channel: req.query.channel
+        })
+        .then( (data,err) => {
+            console.log(data);
+            if(err){
+                console.error(err);
+            }else{
+                res.send(200).send(data);
+            }
+        })
+     } else {
+        res.status(400).send({
+            message: "Wrong phone number" })
+     }
+})
+
+// Verify Endpoint
+app.get('/verify', (req, res) => {
+    if (req.query.mobilenumber && (req.query.code).length === 6) {
+        client
+            .verify
+            .services(config.serviceID)
+            .verificationChecks
+            .create({
+                to: `+${req.query.mobilenumber}`,
+                code: req.query.code
+            })
+            .then(data => {
+                if (data.status === "approved") {
+                    res.status(200).send({
+                        message: "User is Verified!!",
+                        data
+                    })
+                }
+            })
+    } else {
+        res.status(400).send({
+            message: "Wrong phone number or code"
+        })
+    }
+})
+
+// listen to the server at 3000 port
+app.listen(port, () => {
+    console.log(`Server is running at ${port}`)
